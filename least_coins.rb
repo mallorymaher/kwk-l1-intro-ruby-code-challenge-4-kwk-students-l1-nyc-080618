c1fcd3e3d4da288b2fc6454e850bd43542185d8c
mallorymaher <mallory.maher@comcast.net>
2018-08-08 20:18:05 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,9 +2,9 @@ cents = {:quarter => "25", :dime => "10", :nickel => "5", :penny => "1"}
 
 def least_coins(cents)
   x = 189
-  v = x%25
-  puts v
-  w = x-v%10
+    a = x/25
+  b = x%25
+    w = x-v%10
   puts w
   y = x-v-w%5
   puts y
