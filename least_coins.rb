8b928bedd41c87edadc905a3e7f65ad88136b3d6
mallorymaher <mallory.maher@comcast.net>
2018-08-08 20:32:44 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,15 @@
-#write out your code here
+x = 189
 
-def least_coins(cents)
-
-#Code your answer here!
-
+def least_coins(x)
+    a = x/25
+  b = x%25
+    c = b/10
+  d = b%10
+    e = d/5
+  f = d%5
+    g = f/1
+  final = a + c + e + g
+  puts final
 end
+
+least_coins(x)
