2f1fca39233feeb239ef5814dcabe622bfd7da3e
mallorymaher <mallory.maher@comcast.net>
2018-08-08 20:10:37 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,6 +1,9 @@
-coins = {:quarter = "25", :dime = "10", :nickel = "5", :penny = "1"}
+cents = {:quarter = "25", :dime = "10", :nickel = "5", :penny = "1"}
+x = 189
 
 def least_coins(cents)
   v = x/25
-  w
+  w = x/10
+  y = x/5
+  z = x/1
 end
